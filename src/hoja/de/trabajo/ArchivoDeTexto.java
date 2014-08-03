c863ec8dc434e688f02ee8dd1e80ff497af08f1c
Temp <Temp@VAIO>
2014-08-03 01:15:18 -0600

    new commit

--- src/hoja/de/trabajo/ArchivoDeTexto.java
+++ src/hoja/de/trabajo/ArchivoDeTexto.java
@@ -11,15 +11,22 @@ import java.io.*;
  * @author Temp
  */
 public class ArchivoDeTexto {
-    FileWriter fichero = null;
-    PrintWriter pw = null;
+    private FileWriter fichero = null;
+    private PrintWriter pw = null;
+    private File archivo = null;
+    private FileReader fr = null;
+    private BufferedReader br = null;
+    
+    private int[] numero;
         
     public ArchivoDeTexto(){
-        lectura();
+        escritura();
+        numero = new int[2001];
+    
     }
     
-    
-    public void lectura(){
+    //ESTE METODO GENERA LA LISTA DE 2000 NUMEROS ENTEROS ALEATORIOS
+    public void escritura(){
          try
         {
             fichero = new FileWriter("C:\\Users\\Temp\\Desktop\\Cuarto Semestre\\Algoritmos y estructura de datos\\Hoja de trabajo/archivo.txt");
@@ -46,5 +53,43 @@ public class ArchivoDeTexto {
   
         }
     }
+    
+    //ESTE METODO IMPRIME LA LISTA DE 2000 NUMEROS ENTEROS ALEATORIOS Y RETORNA UNA LISTA CON LOS 2000 NUMEROS ENTEROS
+    public int[] lectura(){ 
+ 
+      try {
+         // Apertura del fichero y creacion de BufferedReader para poder
+         // hacer una lectura comoda (disponer del metodo readLine()).
+         archivo = new File ("C:\\Users\\Temp\\Desktop\\Cuarto Semestre\\Algoritmos y estructura de datos\\Hoja de trabajo/archivo.txt");
+         fr = new FileReader (archivo);
+         br = new BufferedReader(fr);
+ 
+         // Lectura del fichero
+         String linea;
+         int i= 0;
+         while((linea=br.readLine())!=null){
+             numero[i]=Integer.parseInt(linea);
+             i++;
+         }
+         return numero;
+            
+      }
+      catch(Exception e){
+         e.printStackTrace();
+      }finally{
+         // En el finally cerramos el fichero, para asegurarnos
+         // que se cierra tanto si todo va bien como si salta 
+         // una excepcion.
+         try{                    
+            if( null != fr ){   
+               fr.close();     
+            }                  
+         }catch (Exception e2){ 
+            e2.printStackTrace();
+         }
+       }
+         return numero;
        
+    }
+   
 }
--- src/hoja/de/trabajo/HojaDeTrabajo.java
+++ src/hoja/de/trabajo/HojaDeTrabajo.java
@@ -6,5 +6,14 @@ public class HojaDeTrabajo {
  
     public static void main(String[] args) {
         ArchivoDeTexto nuevo = new ArchivoDeTexto();
+        
+        //AQUI ESTA PARA PROBAR LA LISTA DE ENTEROS GEBNERA 
+//        int[] lista = new int[2001];
+//        lista = nuevo.lectura();
+//        
+//        for (int s= 0; s<2000;s++){
+//            System.out.println(lista[s]);
+//            
+//        }
     }
 }
